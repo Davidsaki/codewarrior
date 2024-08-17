88aa5f0e62659805da680f4b1bff9759f47e04b4
David Cruz <david.cruz@employbridge.com>
2024-08-17 12:58:32 -0500

    all

---
+++ 4/globalVariables.ts
@@ -0,0 +1,20 @@
+let globalVariables: Record<string, any> = {
+    errorLog: [],
+    events: [],
+};
+
+export function setVariable(key: string, value: string): void {
+    globalVariables[key] = value;
+}
+
+export function getVariable(key: string): any {
+    return globalVariables[key];
+}
+
+export function addToErrorLog(message: string): void {
+    globalVariables.errorLog.push(message);
+}
+
+export function addToEvents(event: string): void {
+    globalVariables.events.push(event);
+}
